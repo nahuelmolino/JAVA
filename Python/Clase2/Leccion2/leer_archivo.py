2890d0223ced48e90d13105f7ad031449a315964
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-20 19:31:24 -0300

    Clase 2 Parte 6 Python

--- Python/Clase2/Leccion2/leer_archivo.py
+++ Python/Clase2/Leccion2/leer_archivo.py
@@ -1,9 +1,11 @@
 
 
-archivo = open('prudeba.txt','r', encoding='utf8') # las letras son: 'r' read, 'a' append, 'w' write, 'x'
-#    print(archivo.read())
-
-
+archivo = open('prueba.txt','r', encoding='utf8') # las letras son: 'r' read, 'a' append, 'w' write, 'x'
+# print(archivo.read())
+# print(archivo.read(16))
+# print(archivo.read(10)) # continua desde la linea anterior.
+print(archivo.readline()) #solo la primera linea
+print(archivo.readline())
 
 
 
