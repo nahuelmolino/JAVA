81e1732d1cade65ab5eb0f0e4b9162f4e283f93a
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-20 17:04:59 -0300

    Clase1 Parte 6 Python

--- Python/Clase1/Leccion01/manejo_excepciones.py
+++ Python/Clase1/Leccion01/manejo_excepciones.py
@@ -1,5 +1,5 @@
 #Capturar error de division por Cero.
-resultado = Nnoe # None significa no valor
+resultado = None # None significa no valor
 
 try:
     a = int(input('Digite el primer número: '))
@@ -11,7 +11,13 @@ except ZeroDivisionError as e:
     print(f'ZeroDivisionError - Ocurrió un error: {type(e)}')
 except Exception as e:
     print(f'Exception - Ocurrió un error: {type(e)}')
+else:
+    print(f'No se arrojo ninguna excepción')
+finally: # Siempre se va a ejecutar
+    print(f'Ejecucion de este bloque finally')
+
 #Procesamiento de excepciones
+
 
 print(f'El resulado es: {resultado}')
 print('seguimos...')
