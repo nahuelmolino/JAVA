720e1a50bbd7bc13f69fd63539728d697a53bca8
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-20 19:59:12 -0300

    Clase 2 Parte 8 Python

--- Python/Clase2/Leccion2/leer_archivo.py
+++ Python/Clase2/Leccion2/leer_archivo.py
@@ -9,7 +9,12 @@ archivo = open('prueba.txt','r',
 # vamos a iterar el archivo, cada una de las líneas.
 # for linea in archivo:
     #   print(linea): iteramos todos los elementos del archivo
-print(archivo.readlines()[11]) # accedemos al archivo como si fuera una lista [valor, valor, ..]
+# print(archivo.readlines()[11]) # accedemos al archivo como si fuera una lista [valor, valor, ..]
+# Anexamos información, copiamos a otro
+archivo2 = open('copia.txt','w', encoding='utf8')
+archivo2.write(archivo.read())
+archivo.close() # cerramos el primer archivo
+archivo2.close() # cerramos el segundo archivo
 
-
+print('Se ha terminado el proceso de leer y copiar archivos')
 
