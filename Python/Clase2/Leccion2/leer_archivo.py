ae5132c9294829d8adb2783da9a313f96b33d941
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-20 19:24:09 -0300

    Clase 2 Parte 5 Python

---
+++ Python/Clase2/Leccion2/leer_archivo.py
@@ -0,0 +1,10 @@
+
+
+archivo = open('prudeba.txt','r', encoding='utf8') # las letras son: 'r' read, 'a' append, 'w' write, 'x'
+#    print(archivo.read())
+
+
+
+
+
+
--- Python/Clase2/Leccion2/manejo_archivos.py
+++ Python/Clase2/Leccion2/manejo_archivos.py
@@ -4,6 +4,8 @@ try:
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n') # el \n es el enter en el txt
     archivo.write('Los acenos son importantes para las palabras\n')
     archivo.write('como por ejemplo: acción, ejecución y producción\n')
+    archivo.write('las letras son:\nr read leer, \na append anexa, \nw write escribe, \nx crea un archivo')
+    archivo.write('\nt esta es para texto o text\nb esta es para binary(imagenes, videos, pdf.)\nw+ lee y escribe r+ \n')
     archivo.write('Con esto terminamos')
 
 except Exception as e:
