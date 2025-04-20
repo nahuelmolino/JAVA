c88fa7d4b893b950597ca4b29a3dc803d69f60c9
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-20 19:01:49 -0300

    Clase 2 Parte 3 Python

--- Python/Clase2/Leccion2/manejo_archivos.py
+++ Python/Clase2/Leccion2/manejo_archivos.py
@@ -1,10 +1,14 @@
 # Declaramos una variable
 try:
-    archivo = open('prueba.txt', 'w') #La w es de write que en ingles significa escribir.
+    archivo = open('prueba.txt', 'w', encoding='utf8') #La w es de write que en ingles significa escribir.
     archivo.write('Programamos con diferentes tipos de archivos, ahora en txt.\n') # el \n es el enter en el txt
+    archivo.write('Los acenos son importantes para las palabras\n')
+    archivo.write('como por ejemplo: acción, ejecución y producción\n')
     archivo.write('Con esto terminamos')
+
 except Exception as e:
     print(e)
 finally: # siempre se ejecuta
     archivo.close() # Con esto se debe cerrar el archivo.
 
+# archivo.write('todo ok'): este es un error porque lo cerre antes.
