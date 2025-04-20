b131a65e0ee46e8a4820f0e79f4324f4d9e29c74
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-20 20:12:50 -0300

    Clase 2 Parte 9 Python

---
+++ Python/Clase2/Leccion2/archivo_con_with.py
@@ -0,0 +1,8 @@
+# MANEJO DE CONTEXTO WITH: sintaxis simplificada, abre y cierra el archivo.
+# uso de la palabra with y el manejo de archivos.. Manejo de contexton with
+with open('prueba.txt', 'r', encoding='utf8') as archivo :
+    print(archivo.read())
+# No hace falta ni el try, ni el finally
+# en el contexto de with lo que se ejecuta de manera automatica
+# Utiliza diferentes métodos: __enter__(abre el recurso)
+# Ahora el siguiente método: __exit__(cierra el recurso)
