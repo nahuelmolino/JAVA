b36570bce16b5ec7d32a9528128e6e3c9b5edee6
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-21 21:53:32 -0300

    Clase02-video2

---
+++ js/clase02/02-ModoStrict.js
@@ -0,0 +1,12 @@
+//va al inicio del programa, dentro de la fn no anda.
+"use strict";
+let x=10;
+console.log(x)
+
+function miFunction(){
+    "use strict";
+    let  y = 10;
+    console.log(y);
+}
+
+miFunction();
