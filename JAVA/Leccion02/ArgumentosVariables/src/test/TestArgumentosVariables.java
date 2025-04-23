d9ca833bf8ca72a7f9b03b8d586e7351532d5555
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-23 20:21:26 -0300

    Leccion02-Video 1y2

--- JAVA/Leccion02/ArgumentosVariables/src/test/TestArgumentosVariables.java
+++ JAVA/Leccion02/ArgumentosVariables/src/test/TestArgumentosVariables.java
@@ -4,10 +4,17 @@ package test;
 public class TestArgumentosVariables {
     public static void main(String[] args) {
      imprimirNumeros(1,2,3,4,8);
-     
      imprimirNumeros(1,28);
+     VariosParametros("Juan","Perez",12,13,25);
      
     }
+    //siempre al final van los argumentos variables
+    private static void VariosParametros(String nombre, String apellido, int ...numeros){
+        System.out.println("Nombre:"  + nombre+ ", Apellido:"  + apellido);
+        //System.out.println("Apellido:"  + apellido);
+        imprimirNumeros(numeros);
+    }
+    
     //tipo ... significa que vamos a recibir argumentos varibales.
     //se convierte en un arreglo. Se trata igual
     private static void imprimirNumeros(int ...numeros){
