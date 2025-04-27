0545bfc8032f438becd1a232fc4954aa70d01875
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-27 20:29:55 -0300

    Clase 3 Parte 2 Java

--- JAVA/Leccion03/ForEach/src/test/TestForEach.java
+++ JAVA/Leccion03/ForEach/src/test/TestForEach.java
@@ -1,17 +1,27 @@
 package test;
 
+import domain.Persona;
+
 public class TestForEach {
+
     public static void main(String[] args) {
-        int edades[]= {5,6,8,9}; // sintaxis resumida
-     //for convencional   
+        int edades[] = {5, 6, 8, 9}; // sintaxis resumida
+        //for convencional   
         for (int i = 0; i < edades.length; i++) {
-            System.out.println("edades["+i+"]" + edades[i] );
+            System.out.println("edades[" + i + "]" + edades[i]);
         }
         //sirve para arreglos, colecciones y listas.
         //definimos una variable donde guarda los valores del arreglo.
-        for(int edad : edades){ //sintaxis del ForEach
+        for (int edad : edades) { //sintaxis del ForEach
             System.out.println("edad = " + edad);
         }
         
+        Persona personas [] = {new Persona("Juan"), new Persona("Carla"), new Persona("Beatriz")};
+        //tipo, variable: arreglo
+        for (Persona per : personas){
+            System.out.println("persona = " + per);
+
+        }
+    
     }
 }
