10048a0c9f6fba383f3e3b13450a6add03e1bf28
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-04-28 21:08:13 -0300

    Clase 2 Parte 11 Python

--- Python/Clase2/Leccion2/archivo_con_with.py
+++ Python/Clase2/Leccion2/archivo_con_with.py
@@ -1,6 +1,8 @@
 from ManejoArchivo import ManejoArchivos
 
 # MANEJO DE CONTEXTO WITH: sintaxis simplificada, abre y cierra el archivo.
+# with ManejoArchivos('prueba.txt') as archivo:
+#   print(archivo.read())
 # uso de la palabra with y el manejo de archivos.. Manejo de contexton with
 #with open('prueba.txt', 'r', encoding='utf8') as archivo :
  # print(archivo.read())
