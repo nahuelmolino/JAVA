3ccb050315156908e33cc91e2ae7db2a411cfe1b
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-05-01 18:55:47 -0300

    Clase 3 Parte 4 Javagit add .!

--- JAVA/Leccion03/AutoboxingUnboxing/src/test/TestAutoBoxingUnboxing.java
+++ JAVA/Leccion03/AutoboxingUnboxing/src/test/TestAutoBoxingUnboxing.java
@@ -14,8 +14,15 @@ public class TestAutoBoxingUnboxing {
         int enteroPrim = 10;// Tipo primitivo
         Integer entero = 10;// Tipo object con la clase Integer
         
+        //el autoboxin convierte un objeto en un tipo primitiv
+        
           System.out.println("entero = " + enteroPrim);
-          System.out.println("entero = " + entero.toString());
+          System.out.println("entero = " + entero.doubleValue());//Autoboxing
+          
+          //unboxing se extrae la lieteral de un objeto entero y se asigna a nuestra variable
+          int entero2 = entero;// Unboxing
+          System.out.println("entero2 = " + entero2);
+          
         
     }
 }
