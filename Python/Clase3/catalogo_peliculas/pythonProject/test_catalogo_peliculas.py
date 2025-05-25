c0da51f1eef01f6fb2e12471417d87fb0128c133
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-05-25 14:24:20 -0300

    Clase 3 Parte 5 Python

--- Python/Clase3/catalogo_peliculas/pythonProject/test_catalogo_peliculas.py
+++ Python/Clase3/catalogo_peliculas/pythonProject/test_catalogo_peliculas.py
@@ -1,5 +1,8 @@
 from logging import exception
 
+from dominio.Pelicula import Pelicula
+from Servicio.catalogo_peliculas import CatalogoPeliculas as cp
+
 opcion = None
 while opcion != 4:
     try:
@@ -9,6 +12,15 @@ while opcion != 4:
         print('3. Eliminar catálogo de peliculas')
         print('4. Salir')
         opcion = int(input('Digite una opción de menú (1-4)'))
+        if opcion ==1:
+            nombre_pelicula = input('Digite el nombre de la pelicula')
+            pelicula  = Pelicula(nombre_pelicula)
+            cp.agregar_peliculas(pelicula)
+        elif opcion == 2:
+            cp.listar_peliculas()
+        elif opcion == 3:
+            cp.eliminar_peliculas()
+
     except Exception as e:
         print(f'Ocurrió un error de tipo: {e}')
         opcion = None
