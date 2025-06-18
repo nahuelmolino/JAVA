b7b252bfd60b7dfcb6490731165ce7ab11ec8618
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-06-18 20:47:16 -0300

    Clase 5 Parte 7 Python

---
+++ Python/Clase4/BD/db/actualizar_registro.py
@@ -0,0 +1,19 @@
+import psycopg2 # Esto es para poder conectarnos a Postgre
+
+# Esto es para poder conectarnos a Postgre
+conexion = psycopg2.connect(    user = 'postgres',    password = 'admin',    host = '127.0.0.1',    port = '5432',    database = 'test_db')
+# print (conexion)
+try:
+    with (conexion):
+        with conexion.cursor() as cursor:
+            sentencia = 'UPDATE persona SET nombre=%s, apellido=%s, email=%s WHERE id_persona=%s'
+            valores = ('juan carlos','Roldan', 'rcarlos.mail@com',2) # Es una tupla
+            cursor.execute(sentencia, valores)  # De esta manera ejecutamos la sentencia
+            registros_actualizados = cursor.rowcount
+            print(f'Los registros actualizados son: {registros_actualizados}')
+
+
+except Exception as e:
+        print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
