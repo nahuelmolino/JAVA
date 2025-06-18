ebcd58ef7a03751178cb09bca487375a432cb917
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-06-17 21:41:12 -0300

    Js clase4

--- js/clase04/ManejoErrores.js
+++ js/clase04/ManejoErrores.js
@@ -3,11 +3,11 @@
 // Veamos como evitar este error
 try{
     let x=10;//Lo traemos con alt + flecha hacia arriba o hacia abajo    
-    miFuncion();
-    throw 'Mi error';
+    //miFuncion();
+    throw 'Mi error'; //Maneja tipo string
 }
 catch(error){
-    console.log(error) 
+    console.log(typeof(error)) 
 }
 finally{
     console.log('Termina la revision de errores')
@@ -16,3 +16,19 @@ finally{
    console.log('Continuamos...') // Esto no se llega a ver porque esta bloqueado
 
    let resultado = 5;
+
+   try{
+        //y = 5;
+        if(isNaN(resultado)) throw 'No es un número';
+        else if(resultado ==='') throw 'Es cadena vacía';
+        else if(resultado >= 0) throw 'Valor Positivo';
+        else if(resultado <= 0) throw 'Valor Negativo';
+   }
+   catch(error){
+        console.log(error);
+        console.log(error.name);
+        console.log(error.message)
+   }
+   finally{
+        console.log('Termina la revisión de errores 2');
+   }
