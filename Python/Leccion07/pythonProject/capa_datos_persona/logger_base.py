094a6e35f8a471d04546beba7f62425b11e5d2e9
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-06-21 17:12:26 -0300

    Clase 7 Parte 4 Python

--- Python/Leccion07/pythonProject/capa_datos_persona/logger_base.py
+++ Python/Leccion07/pythonProject/capa_datos_persona/logger_base.py
@@ -1,8 +1,17 @@
 import logging as log
 
+# docs.python.org/3/howto/logging.html
 # Llamamos una configuración básica
+
+log.basicConfig(level=log.DEBUG,
+                format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',  # hora, nivel, archivo y linea que dio error
+                datefmt='%I:%M:$S %p', # configuramos hora: i= hora, m: minutos, s: segundos p: am o pm
+                handlers=[
+                    log.FileHandler('capa_datos.log'), #nombre de archivo
+                    log.StreamHandler() #consola que venimos manejando
+                ]) # handlers: manejador de
+
 if __name__ == '__main__':
-    log.basicConfig(level=log.DEBUG)
     log.debug('Mensaje a nivel debug')
     log.info('Mensaje a nivel info')
     log.warning('Mensaje a nivel warning')
