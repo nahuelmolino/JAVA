2d8956730c776dab77c1d49c8cfc12dbd578bb01
Nahuel Molino <nahuel.molino.mza@gmail.com>
2025-06-21 12:34:04 -0300

    Clase 6 Parte 5 Python

--- Python/Clase4/BD/db/transacciones.py
+++ Python/Clase4/BD/db/transacciones.py
@@ -4,12 +4,17 @@ from psycopg2.errorcodes import CONNECTION_EXCEPTION
 conexion = bd.connect(user = 'postgres', password = 'admin', host = '127.0.0.1', port = '5432', database = 'test_db')
 
 try:
-    # conexion.autocommit = False # no se guarda de forma automatica # esto directamente no debería estar
+    conexion.autocommit = False # no se guarda de forma automatica # esto directamente no debería estar, inicia la transaccion
     cursor = conexion.cursor()
+
     sentencia = 'INSERT INTO persona(nombre, apellido, email) VALUES (%s,%s,%s)'
-    valores = ('Maria', 'Esparza', 'mesparza@gmail.com')
+    valores = ('Carlos', 'Lara', 'clara@gmail.com')
     cursor.execute(sentencia, valores)
-    conexion.commit() # hacemos el commit manuanlamente
+    sentencia = 'UPDATE persona SET nombre = %s, apellido = %s, email = %s WHERE id_persona = %s'
+    valores = ('Juan', 'Juarez', 'jcjuarez@mail.com',7)
+    cursor.execute(sentencia, valores)
+
+    conexion.commit() # hacemos el commit manuanlamente , se cierra la transaccion
     print('Termina la transacción')
 except Exception as e:
     conexion.rollback() # agrego el rollback
